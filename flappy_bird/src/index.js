312658b9557347c64fa0a8e22b57f6489e571770
suyeon <suyeon@subak-MacBook-Pro.local>
2024-04-11 17:42:34 +0900

    장애물 생성 및 충돌

--- flappy_bird/src/index.js
+++ flappy_bird/src/index.js
@@ -11,11 +11,78 @@ let player = {
     score: 0,
     inplay: true,
 };
+let pipe = {
+    startPos: 0,
+    spaceBetweenRow: 0,
+    spaceBetweenCol: 0,
+    pipeCount: 0,
+};
 
-const playGmaeOver = () => {
+const playGmaeOver = (bird) => {
     player.inplay = false;
+
     gameMessage.classList.remove("hide");
-    gameMessage.innerHTML = `GAME OVER<br/>당신의 점수는 ${player.score}점 입니다.<br/>다시 시작하려면 여기를 누르세요!`;
+    gameMessage.innerHTML = `GAME OVER<br/>당신의 점수는 ${player.score + 1}점 입니다.<br/>다시 시작하려면 여기를 누르세요!`;
+
+    bird.setAttribute("style", "transform: rotate(180deg)");
+};
+
+const makePipe = (pipePos) => {
+    let totalHeight = gameArea.offsetHeight;
+    let totalWidth = gameArea.offsetWidth;
+
+    let pipeUp = document.createElement("div");
+    pipeUp.classList.add("pipe");
+    pipeUp.height = Math.floor(Math.random() * 350);
+    pipeUp.style.height = pipeUp.height + "px";
+    pipeUp.style.left = totalWidth + pipePos + "px";
+    pipeUp.x = totalWidth + pipePos;
+    pipeUp.style.top = "0px";
+    pipeUp.style.backgroundColor = "red";
+    gameArea.appendChild(pipeUp);
+
+    pipe.spaceBetweenCol = Math.floor(Math.random() * 250) + 150;
+
+    let pipeDown = document.createElement("div");
+    pipeDown.classList.add("pipe");
+    pipeDown.style.height = totalHeight - pipeUp.height - pipe.spaceBetweenCol + "px";
+    pipeDown.style.left = totalWidth + pipePos + "px";
+    pipeDown.x = totalWidth + pipePos;
+    pipeDown.style.bottom = "0px";
+    pipeDown.style.backgroundColor = "black";
+    gameArea.appendChild(pipeDown);
+};
+
+const isCollide = (pipe, bird) => {
+    let pipeRect = pipe.getBoundingClientRect();
+    let birdRect = bird.getBoundingClientRect();
+    return (
+        pipeRect.bottom > birdRect.top &&
+        pipeRect.top < birdRect.bottom &&
+        pipeRect.left < birdRect.right &&
+        pipeRect.right > birdRect.left
+    );
+}
+
+const movePipes = (bird) => {
+    let pipes = document.querySelectorAll(".pipe");
+    let counter = 0;
+    pipes.forEach((item) => {
+        item.x -= player.speed;
+        item.style.left = item.x + "px";
+        if (item.x < 0) {
+            item.parentElement.removeChild(item);
+            counter++;
+        }
+
+        if (isCollide(item, bird)) {
+            playGmaeOver(bird);
+        }
+    });
+
+    for (let i = 0; i < counter / 2; i++) {
+        makePipe(0)
+    }
 };
 
 const playGame = () => {
@@ -23,6 +90,8 @@ const playGame = () => {
         let move = false;
         let bird = document.querySelector(".bird");
         let wing = document.querySelector(".wing");
+
+        movePipes(bird);
 
         if (keys.ArrowLeft && player.x > 0) {
             player.x -= player.speed;
@@ -47,7 +116,7 @@ const playGame = () => {
         }
 
         if (player.y > gameArea.offsetHeight) {
-            playGmaeOver();
+            playGmaeOver(bird);
         }
 
         player.y += player.speed * 2;
@@ -85,6 +154,14 @@ const start = () => {
     player.x = bird.offsetLeft;
     player.y = bird.offsetTop;
 
+    pipe.startPos = 0;
+    pipe.spaceBetweenRow = 400;
+    pipe.pipeCount = Math.floor(gameArea.offsetWidth / pipe.spaceBetweenRow);
+    for (let i = 0; i < pipe.pipeCount; i++) {
+        makePipe(pipe.startPos * pipe.spaceBetweenRow);
+        pipe.startPos++;
+    }
+
     window.requestAnimationFrame(playGame);
 };
 startBtn.addEventListener("click", start);
@@ -92,12 +169,10 @@ gameMessage.addEventListener("click", start);
 
 const pressOn = (e) => {
     keys[e.code] = true;
-    console.log(keys);
 };
 document.addEventListener("keydown", pressOn);
 
 const pressOff = (e) => {
     keys[e.code] = false;
-    console.log(keys);
 };
 document.addEventListener("keyup", pressOff);
